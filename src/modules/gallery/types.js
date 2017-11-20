068a65f1bd30752db413d434e70b7e2990a92290
rsmelo <rodrigosm87@gmail.com>
2017-11-20 17:14:19 -0200

    gallery: add types

---
+++ src/modules/gallery/types.js
@@ -0,0 +1,5 @@
+const namespace = 'GALLERY'
+export const GET_PRODUCTS = `${namespace}/GET_PRODUCTS`
+export const PRODUCTS_REQUEST = `${namespace}/PRODUCTS_REQUEST`
+export const PRODUCTS_SUCCESS = `${namespace}/PRODUCTS_SUCCESS`
+export const PRODUCTS_FAILURE = `${namespace}/PRODUCTS_FAILURE`
