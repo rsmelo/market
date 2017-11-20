656883a62b8cafddeb7f103149d5f26fb4b76849
rsmelo <rodrigosm87@gmail.com>
2017-11-20 15:00:42 -0200

    router: add Gallery to routes

--- src/router.js
+++ src/router.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import Gallery from './modules/gallery'
 
 const basename = '/'
 
 export default (
   <BrowserRouter basename={basename} >
     <Switch>
-      <Route exact path="/" component={() => (<h1>Gallery</h1>)} />
+      <Route exact path="/" component={Gallery} />
       <Route exact path="/product/:id" component={() => (<h1>product</h1>)} />
       <Route exact path="/cart" component={() => (<h1>cart</h1>)} />
     </Switch>
