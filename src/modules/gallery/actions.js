1adff689f67fd3b63b4a73c9560eeb7a5c07f32c
rsmelo <rodrigosm87@gmail.com>
2017-11-20 17:15:01 -0200

    gallery: add actions to fetch products

---
+++ src/modules/gallery/actions.js
@@ -0,0 +1,13 @@
+import { createAction } from 'redux-actions'
+
+import {
+  GET_PRODUCTS,
+  PRODUCTS_REQUEST,
+  PRODUCTS_SUCCESS,
+  PRODUCTS_FAILURE,
+} from './types'
+
+export const getProducts = createAction(GET_PRODUCTS)
+export const productsRequest = createAction(PRODUCTS_REQUEST)
+export const productsSucces = createAction(PRODUCTS_SUCCESS)
+export const productsFailure = createAction(PRODUCTS_FAILURE)
