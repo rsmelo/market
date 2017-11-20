c57a80e526d47ae8442ecc14730e9c9c7e1714e5
rsmelo <rodrigosm87@gmail.com>
2017-11-20 17:15:53 -0200

    gallery: add reducer and selectors to combinedReducers

--- src/modules/combinedReducers.js
+++ src/modules/combinedReducers.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux'
 
-const rootReducer = combineReducers({})
+import gallery, * as fromGallery from './gallery/reducer'
+
+const rootReducer = combineReducers({ gallery })
 
 export default rootReducer
+
+export const getGalleryProducts = state => fromGallery.getProducts(state.gallery)
+export const getGalleryIsFetching = state => fromGallery.getIsFetching(state.gallery)
