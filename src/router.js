24573cde33ff2670b2f7f5f32e456e33854ea1ec
rsmelo <rodrigosm87@gmail.com>
2017-11-21 17:28:13 -0200

    general: add Product to router

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Gallery from './modules/gallery'
+import Product from './modules/product'
 
 const basename = '/'
 
@@ -9,7 +10,7 @@ export default (
   <BrowserRouter basename={basename} >
     <Switch>
       <Route exact path="/" component={Gallery} />
-      <Route exact path="/product/:id" component={() => (<h1>product</h1>)} />
+      <Route exact path="/product/:id" component={Product} />
       <Route exact path="/cart" component={() => (<h1>cart</h1>)} />
     </Switch>
   </BrowserRouter>
