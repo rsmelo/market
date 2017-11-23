71e082a59180c9ea893839e4b2fc357948d71949
rsmelo <rodrigosm87@gmail.com>
2017-11-23 01:23:21 -0200

    product: add fetchProduct mock api

---
+++ src/modules/product/api.js
@@ -0,0 +1,15 @@
+import productsJSON from '../../mocks/products.json'
+
+const fetchProduct = id => new Promise((resolve) => {
+  setTimeout(() => {
+    const product = productsJSON.find(item => item.id === id)
+    const response = {
+      statusCode: 200,
+      status: '200 OK',
+      data: product,
+    }
+    resolve(response)
+  }, 100)
+})
+
+export default fetchProduct
