66f8257b65e35d215ede007d2017c00000fa5eae
rsmelo <rodrigosm87@gmail.com>
2017-11-23 01:04:32 -0200

    product: add actions and types

---
+++ src/modules/product/types.js
@@ -0,0 +1,5 @@
+const namespace = 'PRODUCT'
+export const GET_PRODUCT = `${namespace}/GET_PRODUCT`
+export const PRODUCT_REQUEST = `${namespace}/PRODUCT_REQUEST`
+export const PRODUCT_SUCCESS = `${namespace}/PRODUCT_SUCCESS`
+export const PRODUCT_FAILURE = `${namespace}/PRODUCT_FAILURE`
