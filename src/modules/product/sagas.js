67955a7a30228376d1e3a93429474a880ef1132c
rsmelo <rodrigosm87@gmail.com>
2017-11-23 01:22:52 -0200

    product: add sagas

---
+++ src/modules/product/sagas.js
@@ -0,0 +1,18 @@
+import { takeLatest, put, call } from 'redux-saga/effects'
+
+import * as actions from './actions'
+import fetchProduct from './api'
+
+export function* getProduct ({ payload }) {
+  try {
+    yield put(actions.productRequest())
+    const { data } = yield call(fetchProduct, payload.id)
+    yield put(actions.productSuccess(data))
+  } catch (error) {
+    yield put(actions.productFailure(error.message))
+  }
+}
+
+export default function* sagas () {
+  yield takeLatest(actions.getProduct, getProduct)
+}
