8e2d20c90eff414f003d9a3c04914a2a96d848fb
rsmelo <rodrigosm87@gmail.com>
2017-11-23 01:26:24 -0200

    general: add productSagas to rootSaga

--- src/sagas.js
+++ src/sagas.js
@@ -1,9 +1,11 @@
 import { fork, all } from 'redux-saga/effects'
 
 import gallerySagas from './modules/gallery/sagas'
+import productSagas from './modules/product/sagas'
 
 export default function* rootSaga () {
   yield all([
     fork(gallerySagas),
+    fork(productSagas),
   ])
 }
