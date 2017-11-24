643431b4aaf26ccef945dca21fa4b9a38d0647d9
rsmelo <rodrigosm87@gmail.com>
2017-11-24 17:24:58 -0200

    gallery: add snapshot teste to ProductList

--- src/modules/gallery/ProductList/ProductList.test.js
+++ src/modules/gallery/ProductList/ProductList.test.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import renderer from 'react-test-renderer'
 import { shallow } from 'enzyme'
 
 import ProductList from './ProductList'
 import Product from './Product'
 
+jest.mock('react-router-dom', () => ({ Link: 'Link' }))
+
 describe('ProductList', () => {
+  it('should render correctly', () => {
+    const getProducts = jest.fn()
+    const products = [
+      {
+        id: 'a12090',
+        name: 'a product',
+        price: 10,
+        image: 'image-url',
+        description: 'test',
+      },
+      {
+        id: 'a120902',
+        name: 'a product2',
+        price: 10,
+        image: 'image-url2',
+        description: 'test',
+      },
+    ]
+
+    const tree = renderer
+      .create(<ProductList getProducts={getProducts} products={products} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
   it('should call getProducts when mount', () => {
     const getProducts = jest.fn()
     shallow(<ProductList getProducts={getProducts} />)
