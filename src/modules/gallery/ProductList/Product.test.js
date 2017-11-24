e18fa93aee760178bfa4a29a9e94c08c064c73eb
rsmelo <rodrigosm87@gmail.com>
2017-11-24 17:20:20 -0200

    gallery: refactor Product.test

--- src/modules/gallery/ProductList/Product.test.js
+++ src/modules/gallery/ProductList/Product.test.js
@@ -6,18 +6,13 @@ import Product from './Product'
 jest.mock('react-router-dom', () => ({ Link: 'Link' }))
 
 describe('Product', () => {
-  let props
-
-  beforeEach(() => {
-    props = {
+  it('renders correctly', () => {
+    const props = {
       id: 'a12090',
       name: 'a product',
       price: 10,
       image: 'image-url',
     }
-  })
-
-  it('renders correctly', () => {
     const tree = renderer
       .create(<Product {...props} />)
       .toJSON()
