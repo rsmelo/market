4327a1b42ad49147bfdbc2e2f88b0f1fc4d21364
rsmelo <rodrigosm87@gmail.com>
2017-11-29 13:57:13 -0200

    rott: fix router usage

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -1,22 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 
 import router from '../../router'
-
-const basename = '/'
 
 const Root = ({ store }) => (
   <div>
     <Provider store={store}>
-      <BrowserRouter
-        basename={basename}
-      >
-        <div>
-          {router}
-        </div>
-      </BrowserRouter>
+      <div>
+        {router}
+      </div>
     </Provider>
   </div>
 )
