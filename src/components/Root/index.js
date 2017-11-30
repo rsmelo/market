d1af6e95032b7237a0b318323d4c6e1ea67f8a85
rsmelo <rodrigosm87@gmail.com>
2017-11-30 01:34:19 -0200

    root: change root structure

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
+import Header from '../Header'
 import router from '../../router'
+import style from './style.css'
 
 const Root = ({ store }) => (
-  <div>
-    <Provider store={store}>
-      <div>
+  <Provider store={store}>
+    <BrowserRouter>
+      <div className={style.root}>
+        <Header />
         {router}
       </div>
-    </Provider>
-  </div>
+    </BrowserRouter>
+  </Provider>
 )
 
 Root.propTypes = {
--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Gallery from './modules/gallery'
 import Product from './modules/product'
@@ -7,16 +7,12 @@ import Cart from './modules/cart'
 import Checkout from './modules/checkout'
 import Order from './modules/order'
 
-const basename = '/'
-
 export default (
-  <BrowserRouter basename={basename} >
-    <Switch>
-      <Route exact path="/" component={Gallery} />
-      <Route exact path="/product/:id" component={Product} />
-      <Route exact path="/cart" component={Cart} />
-      <Route exact path="/checkout/:id" component={Checkout} />
-      <Route exact path="/order/:id" component={Order} />
-    </Switch>
-  </BrowserRouter>
+  <Switch>
+    <Route exact path="/" component={Gallery} />
+    <Route exact path="/product/:id" component={Product} />
+    <Route exact path="/cart" component={Cart} />
+    <Route exact path="/checkout/:id" component={Checkout} />
+    <Route exact path="/order/:id" component={Order} />
+  </Switch>
 )
